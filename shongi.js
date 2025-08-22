acd254a2d0274edd3d7645cf2003b93a5b1db806
shongibugga <lokiincharge25+1@gmail.com>
2025-08-21 22:54:20 -0400

    Update after latest change

--- shongi.js
+++ shongi.js
@@ -5,7 +5,6 @@
 // @description  Enhancements for SimCompanies web game. Complies with scripting rules of the game.
 // @author       Loki Clarke
 // @match        https://www.simcompanies.com/*
-// @require      https://raw.githubusercontent.com/badtothebone430/chartjs/main/shongi.js
 // @require      https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@3.0.0/dist/chartjs-adapter-date-fns.bundle.min.js
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
